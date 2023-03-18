b0f8f5d8e617211bafc56a9c5a8642f4d8ab4487
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2023-03-18 12:54:26 +0330

    session 2 files

---
+++ s_002/variables.py
@@ -0,0 +1,69 @@
+# name : str = 'ali'
+
+# print(type(name))
+
+
+# name = 983112987
+
+# print(name)
+
+# print(type(name))
+
+# number = 9931112009
+
+# print(number)
+
+# print(type(name))
+
+
+# _student_number = 983117656
+
+# print(_student_number)
+
+
+# my_var = 1
+
+# my_var = True
+
+# my_var = -1
+
+# my_var = 3.14
+
+# my_var = 32 + 4j
+
+# my_var = 'New York'
+#my_var = "nEw YOrk"
+
+# print(len(my_var))
+
+
+#var_without_space = my_var.replace(' ', '')
+
+#print(var_without_space)
+#print(len(var_without_space))
+
+"""
+print(my_var.upper())
+print(my_var.lower())
+print(my_var.title())
+"""
+
+my_var = [ 1 , 'ali' , True , 4 ]
+
+print(my_var)
+
+# print(len(my_var))
+# print(my_var)
+
+# my_var.append(5, 2)
+
+# my_var.pop(1)
+
+# print(len(my_var))
+# print(my_var)
+
+print(my_var[0] + my_var[3])
+
+# print(type(my_var))
+
+
